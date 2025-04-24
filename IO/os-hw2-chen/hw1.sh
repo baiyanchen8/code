5c15191ad5acb63d2c5bada387d1040c71d8b49e
baiyanchen8 <132753198+baiyanchen8@users.noreply.github.com>
2025-04-24 23:00:53 +0800

    kk

--- IO/os-hw2-chen/hw1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for size in 10 20 30 40 50 60 70 80 90 100 200 300 400 500 600 700 800 900 1000
-do
-  echo "${size}MB"
-  echo -e "${size}\n1\n2\n3\n4\n3\n8\n3\n16\n3\n32\n0" | ./hw1
-done
--- IO/os-hw2-chen/hw2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for size in 10 20 30 40 50 60 70 80 90 100 200 300 400 500 600 700 800 900 1000 10
-do
-  echo "${size}MB"
-  echo -e "${size}\n3\n4\n4\n4\n8\n4\n16\n4\n32\n0" | ./hw2
-done
